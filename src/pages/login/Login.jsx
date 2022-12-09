6b9fc117d981313c2ea4635a22575f95f2af29d9
debojit <saha05debojit@gmail.com>
2022-12-10 01:58:33 +0530

    Login Page UI

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World..!</h1>
+          <ul>
+            <li>Connect with your friends & family.</li>
+            <li>Share a glimpse of your favourite moments.</li>
+            <li>
+              Let your friends know about your day through status updates.
+            </li>
+          </ul>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login.</h1>
+          <form>
+            <input type="text" placeholder="abc@email.com" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
