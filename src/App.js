fc8dc8c8733d47bffb208cc800bb4c32e9940bf6
debojit <saha05debojit@gmail.com>
2022-12-10 02:22:18 +0530

    Register Page UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import "./register.scss"
+import React from "react";
+import "./register.scss";
 
 const Register = () => {
   return (
-    <div className='register'>Register</div>
-  )
-}
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Register.</h1>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div className="right">
+          <h1>Hello World..!</h1>
+          <ul>
+            <li>Connect with your friends & family.</li>
+            <li>Share a glimpse of your favourite moments.</li>
+            <li>
+              Let your friends know about your day through status updates.
+            </li>
+          </ul>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
