01607096f631d69d1a880e1810185abfa6c28298
debojit <saha05debojit@gmail.com>
2022-12-14 02:53:47 +0530

    Profile page done

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,86 @@
-import React from 'react'
-import "./profile.scss"
+import React from "react";
+import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
   return (
-    <div className='profile'>Profile</div>
-  )
-}
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="profilePic"
+        />
+      </div>
 
-export default Profile
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a
+              href="http://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a
+              href="http://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a
+              href="http://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a
+              href="http://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedInIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>John Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>john.dev</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
